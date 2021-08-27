9ab919ca9fbf1b8b5c55dd05cdccaa6de2ff25f2
travis <trav.horn@gmail.com>
2021-08-26 21:26:11 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "gatsby-intro-tour-de-fat",
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-gatsby-cloud"],
+};
